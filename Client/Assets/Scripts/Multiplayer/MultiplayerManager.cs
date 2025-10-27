668201bbd44cb5966a4703d686bf23216cdb9a05
Daniil <daniilkhokho@gmail.com>
2025-10-27 01:25:38 +0100

    InitialConnection

--- Client/Assets/Scripts/Multiplayer/MultiplayerManager.cs
+++ Client/Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -1,7 +1,12 @@
 using Colyseus;
+using System;
+using UnityEngine;
 
 public class MultiplayerManager : ColyseusManager<MultiplayerManager>
 {
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _enemy;
+
     private ColyseusRoom<State> _room;
     protected override void Awake()
     {
@@ -14,6 +19,31 @@ public class MultiplayerManager : ColyseusManager<MultiplayerManager>
     private async void Connect()
     {
        _room = await Instance.client.JoinOrCreate<State>("state_handler");
+
+       _room.OnStateChange += OnChange;
+    }
+
+    private void OnChange(State state, bool isFirstState)
+    {
+        state.players.ForEach(ForEachEnemy);
+        if (isFirstState == false) return;
+
+
+        var player = state.players[_room.SessionId];
+        var position = new Vector3(player.x - 200, 0, player.y - 200) / 8;
+
+        Instantiate(_player, position, Quaternion.identity);
+
+        state.players.ForEach(ForEachEnemy);
+    }
+
+    private void ForEachEnemy(string key, Player player)
+    {
+        if (key == _room.SessionId) return;
+
+        var position = new Vector3(player.x - 200, 0, player.y - 200) / 8;
+
+        Instantiate(_enemy, position, Quaternion.identity);
     }
 
     protected override void OnDestroy()
