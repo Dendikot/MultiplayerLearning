9c967d6c0208b40712af1e197fdd84a46683cae9
Daniil <daniilkhokho@gmail.com>
2025-11-12 23:01:57 +0100

    RotationUpdate

--- Client/Assets/Scripts/PlayerCharacter.cs
+++ Client/Assets/Scripts/PlayerCharacter.cs
@@ -4,24 +4,53 @@ public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 2f;
+    [SerializeField] private Transform _head;
+    [SerializeField] private Transform _cameraPoint;
+    [SerializeField] private float _minHeadAngle = -90f;
+    [SerializeField] private float _maxHeadAngle = 90f;
     private float _inputH;
     private float _inputV;
+    private float _rotateY;
+    private float _currentRotateX;
 
-    public void SetInput(float h, float v)
+    private void Start()
+    {
+        Transform camera = Camera.main.transform;
+        camera.parent = _cameraPoint;
+        camera.localRotation = Quaternion.identity;
+        camera.localPosition = Vector3.zero;
+    }
+
+
+    public void SetInput(float h, float v, float rotateY)
     {
         _inputV = v;
         _inputH = h;
+        _rotateY += rotateY;
     }
 
     void FixedUpdate()
     {
         Move();
+        RotateY();
     }
 
     private void Move()
     {
         Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
         transform.position += direction * Time.deltaTime * _speed;
+    }
+
+    private void RotateY()
+    {
+        _rigidbody.angularVelocity = new Vector3(0, _rotateY, 0);
+        _rotateY = 0;
+    }
+
+    public void RotateX(float value)
+    {
+        _currentRotateX = Mathf.Clamp(_currentRotateX + value, _minHeadAngle, _maxHeadAngle);
+        _head.localEulerAngles = new Vector3(_currentRotateX, 0, 0);
     }
 
     public void GetMoveInfo(out Vector3 position, out Vector3 velocity)
